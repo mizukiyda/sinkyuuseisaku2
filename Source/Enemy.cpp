073bbaf764dcb95cf2821ac5e4480e492aa9585d
GM2180136\nwuser <mizuki060411@gmail.com>
2019-05-15 08:49:43 +0900

    [add]Enemyのalive と deathの表示
    中山瑞基

--- Source/Enemy.cpp
+++ Source/Enemy.cpp
@@ -8,22 +8,43 @@
 
 int Enemyalive;          //“G‚Ì‰æ‘œ‚Ì•Ï”(¶)
 int Enemydeath;          //“G‚Ì‰æ‘œ‚Ì•Ï”(€)
-
+int Enemy_OnActive;		//Enemy‚Ì¶‘¶Šm”F
 
 int Enemy_Init() {
-	Enemyalive=LoadGraph("Resource/Image/Enemyalive.png");
-	Enemydeath=LoadGraph("Resource/Image/Enemydeath.png");
+	Enemyalive = LoadGraph("Resource/Image/Enemyalive.png");
+	Enemydeath = LoadGraph("Resource/Image/Enemydeath.png");
+
+	Enemy_OnActive = true;		//¶‚«‚Ä‚¢‚é
 	return 0;
 }
 
 int Enemy_Dpct() {
-
+	if (Enemy_OnActive == false) {
+		if (Keyboard_Get(KEY_INPUT_SPACE) == 1)
+		{
+			Enemy_OnActive = true;
+		}
+	}
+	else
+	{
+		if (Keyboard_Get(KEY_INPUT_SPACE) == 1)
+		{
+			Enemy_OnActive = false;
+		}
+	}
 	
 	return 0;
 }
 
 int Enemy_Draw() {
-	DrawGraph(100, 100, Enemyalive, true);
+	if (Enemy_OnActive == true) 
+	{
+		DrawGraph(100, 100, Enemyalive, true);
+	}
+	else 
+	{
+		DrawGraph(100, 100, Enemydeath, true);
+	}
 	return 0;
 }
 int Enemy_End() {
