0ed29e7e77794561f42698c282075e31c0166b17
ysaito <ysaito.yda@gmail.com>
2019-05-17 11:15:05 +0900

    [add]Enemyの改良修正

--- Source/Enemy.cpp
+++ Source/Enemy.cpp
@@ -9,8 +9,11 @@
 int Enemyalive;          //敵の画像の変数(生)
 int Enemydeath;          //敵の画像の変数(死)
 int Enemy_OnActive;		 //Enemyの生存確認
-int Enemy_x =100;
+
+int Enemy_x = 100;       //
 int Enemy_y = 100;
+int player_x;
+int player_y;
 int E_Rand;
 
 int Enemy_Init() {
@@ -18,7 +21,7 @@ int Enemy_Init() {
 	Enemyalive = LoadGraph("Resource/Image/Enemyalive.png");
 	Enemydeath = LoadGraph("Resource/Image/Enemydeath.png");
 
-	Enemy_OnActive = true;		//生きている
+	Enemy_OnActive = true;		//生死判定 == 生きている
 
 	E_Rand = GetRand(5);
 
@@ -26,6 +29,7 @@ int Enemy_Init() {
 }
 
 int Enemy_Dpct() {
+	/*
 	if (Enemy_OnActive == false) {
 		if (Keyboard_Get(KEY_INPUT_SPACE) == 1)
 		{
@@ -35,31 +39,62 @@ int Enemy_Dpct() {
 	else
 	{
 		if (Keyboard_Get(KEY_INPUT_SPACE) == 1)
-		{ 
+		{
+			Enemy_OnActive = true;
+		}
+		return 0;
 	}
-
-	return 0;
-	while (1) {
-
+	while(1){
 		switch (E_Rand) {
 		case E_Drct_Up:
-			Enemy_y -= 3;
+			Enemy_y --;
 			break;
 		case E_Drct_Down:
-			Enemy_y += 3;
+			Enemy_y ++;
 			break;
 		case E_Drct_Right:
-			Enemy_x += 3;
+			Enemy_x ++;
 			break;
 		case E_Drct_Left:
-			Enemy_x -= 3;
+			Enemy_x --;
 			break;
 		case E_Drct_Stop:
 			break;
 		}
-	}
 	return 0;
 }
+	*/
+	while (1) {                //無限ループ
+		switch (E_Rand) {      
+		case E_Drct_Up:        //上
+			Enemy_y--;
+			break;
+		case E_Drct_Down:      //下
+			Enemy_y++;
+			break;
+		case E_Drct_Right:     //右
+			Enemy_x++;
+			break;
+		case E_Drct_Left:      //左
+			Enemy_x--;
+			break;
+		case E_Drct_Stop:      //止
+			break;
+		}
+		return 0;
+		if (player_x + player_y >= Enemy_x && player_x <= Enemy_x + Enemy_y) {      //プレイヤーとの当たり判定
+			Enemy_OnActive = false;                                                 //当たったら生死判定をfalseにする
+		}
+		break;
+	}
+}
+	/*
+	else
+	{
+		Enemy_OnActive = false;
+	}
+	*/
+
 int Enemy_Draw() {
 	if (Enemy_OnActive == true) 
 	{
