87e0c5114e225fba4ed22c3c26ba7ab56a97b603
ysaito <ysaito.yda@gmail.com>
2019-05-24 09:30:12 +0900

    [add]ソースをいじった

--- Source/Enemy.cpp
+++ Source/Enemy.cpp
@@ -3,19 +3,18 @@
 #include"Enemy.h"
 #include"MAP.h"
 #include"Player.h"
-#include"Keyboard.h"
 
 
 int Enemyalive;          //敵の画像の変数(生)
 int Enemydeath;          //敵の画像の変数(死)
 
 
-int Enemy_x = 100;       //
-int Enemy_y = 100;
-int player_x;
-int player_y;
+int Enemy_x = Enemy_Pos_Init_x();       //
+int Enemy_y = Enemy_Pos_Init_y();
+int Player_x;
+int Player_y;
 int E_Rand;
-
+int Enemy_OnActive;
 
 int Enemy_Init() {
 
@@ -32,77 +31,82 @@ int Enemy_Init() {
 int Enemy_Dpct() {
 	/*
 	if (Enemy_OnActive == false) {
-		if (Keyboard_Get(KEY_INPUT_SPACE) == 1)
-		{
-			Enemy_OnActive = true;
-		}
+	if (Keyboard_Get(KEY_INPUT_SPACE) == 1)
+	{
+	Enemy_OnActive = true;
+	}
 	}
 	else
 	{
-		if (Keyboard_Get(KEY_INPUT_SPACE) == 1)
-		{
-			Enemy_OnActive = true;
-		}
-		return 0;
+	if (Keyboard_Get(KEY_INPUT_SPACE) == 1)
+	{
+	Enemy_OnActive = true;
 	}
-	while (1) {                //無限ループ
-		switch (E_Rand) {
-		case 0:        //上
-			Enemy_y--;
-			break;
-		case 1:      //下
-			Enemy_y++;
-			break;
-		case 2:     //右
-			Enemy_x++;
-			break;
-		case 3:      //左
-			Enemy_x--;
-			break;
-		case 4:      //止
-			break;
-		}
-		return 1;
-		if (player_x + player_y >= Enemy_x && player_x <= Enemy_x + Enemy_y) {      //プレイヤーとの当たり判定
-			Enemy_OnActive = false;                                                 //当たったら生死判定をfalseにする
-		}
-		break;
+	return 0;
 	}
 	*/
+	//while (0) {                //無限ループ
+	switch (E_Rand) {
+	case 0:        //上
+	Enemy_y--;
+	break;
+	case 1:      //下
+	Enemy_y++;
+	break;
+	case 2:     //右
+	Enemy_x++;
+	break;
+	case 3:      //左
+	Enemy_x--;
+	break;
+	case 4:      //止
+	break;
+	}
+	return 1;
+	if (Player_x + Player_y >= Enemy_x && Player_x <= Enemy_x + Enemy_y) {      //プレイヤーとの当たり判定
+	Enemy_OnActive = false;                                                 //当たったら生死判定をfalseにする
+	}
+	//break;
+	//}
+	return 0;
 }
 //Enemyの移動を一つ一つ関数で管理する
 
-int Enemy_Move0() {
-	if()
+int   Enemy_Move0() {
+	if (Enemy_x >200) {
+		Enemy_y++;
+		return 0;
+	}
+}
+int  Enemy_Move1() {
 	return 0;
 }
-int Enemy_Move1() {
+int  Enemy_Move2() {
 	return 0;
 }
-int Enemy_Move2() {
+int  Enemy_Move3() {
 	return 0;
 }
-int Enemy_Move3() {
-	return 0;
-}
-int Enemy_Move4() {
+int  Enemy_Move4() {
 	return 0;
 }
 //	switch (E_Rand) {
 int Enemy_Pos_Init_x() {
-	return Enemy_y;
-}
-int Enemy_Pos_Init_y() {
+	Enemy_x = 100;
 	return Enemy_x;
 }
+int Enemy_Pos_Init_y() {
+	Enemy_y = 100;
+	return Enemy_y;
+}
 int Enemy_Draw() {
-	if (Enemy_OnActive == true) 
+	if (Enemy_OnActive == true)
 	{
 		DrawGraph(Enemy_x, Enemy_y, Enemyalive, true);
 	}
-	else 
+	else
 	{
-		DrawGraph(Enemy_x,Enemy_y, Enemydeath, true);
+		DrawGraph(Enemy_x, Enemy_y, Enemydeath, true);
 	}
 	return 0;
 }
