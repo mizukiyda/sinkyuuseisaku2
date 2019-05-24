4abf11d693ab840812fd117fc8ab7c1b5f3fdb9b
MisimaHikoya <v3jh9xjm@gmail.com>
2019-05-24 10:22:31 +0900

    [add]UI画像表示

--- Source/UI.cpp
+++ Source/UI.cpp
@@ -10,14 +10,15 @@
 
 //ここで変数を宣言（C++を使わないのでグローバル変数）
 int ImageMenu = 0;
-
-
+int Menyu_OnActive;
+int Menyu_x;
+int Menyu_y;
 
 int UI_Init() {
 	//ここで初期化をする
 	//画像の読み込み
 	ImageMenu = LoadGraph("resource/Image/STR.png");
-	
+	Menyu_OnActive = false;  //画像非表示
 	return 0;
 }
 
@@ -25,11 +26,12 @@ int UI_Dpct() {
 	//ここで計算
 	//Dqctは毎フレーム呼ばれる
 	int Key;
+	
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
 		Key = GetJoypadInputState(DX_INPUT_KEY);
 		if (Keyboard_Get(KEY_INPUT_M) == 1) {
-
+			Menyu_OnActive = true;	//画像表示
 		}
 
 	}
@@ -39,9 +41,10 @@ int UI_Dpct() {
 int UI_Draw() {
 	//ここで描写
 	//こっちも毎フレーム呼ばれますが計算とは別に書きます
-	
-
-	
+	if (Menyu_OnActive == true)
+	{
+		DrawGraph(Menyu_x, Menyu_y, ImageMenu, true);
+	}
 
 	return 0;
 }
