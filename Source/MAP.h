9eaf64001261490eee5fee618354eb993018fa05
MisimaHikoya <v3jh9xjm@gmail.com>
2019-05-27 09:26:44 +0900

    [add]マップサイズの変更

--- Source/MAP.h
+++ Source/MAP.h
@@ -3,8 +3,8 @@
 
 
 #define MAP_SIZE	64			// マップチップ一つのドットサイズ
-#define MAP_WIDTH	15			// マップの幅
-#define MAP_HEIGHT	10			// マップの縦長さ
+#define MAP_WIDTH	23			// マップの幅
+#define MAP_HEIGHT	12			// マップの縦長さ
 
 #define MAP_MAX 3
 
